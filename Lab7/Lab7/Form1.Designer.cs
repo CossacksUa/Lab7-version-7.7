d0cd2fb4850f20b81bb1ecd967a4b7c31a596585
CossacksUa <101990398+CossacksUa@users.noreply.github.com>
2024-04-21 21:52:20 +0300

    Добавьте файлы проекта.

---
+++ Lab7/Lab7/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿// Ваш Form1.Designer.cs
+
+namespace Lab7
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnCenter;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.Button btnItalic;
+        private System.Windows.Forms.Button btnUnderline;
+        private System.Windows.Forms.ComboBox comboBoxLanguages;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnCenter = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.btnUnderline = new System.Windows.Forms.Button();
+            this.comboBoxLanguages = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 397);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(12, 12);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 1;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnCenter
+            // 
+            this.btnCenter.Location = new System.Drawing.Point(93, 12);
+            this.btnCenter.Name = "btnCenter";
+            this.btnCenter.Size = new System.Drawing.Size(75, 23);
+            this.btnCenter.TabIndex = 2;
+            this.btnCenter.Text = "Center";
+            this.btnCenter.UseVisualStyleBackColor = true;
+            this.btnCenter.Click += new System.EventHandler(this.btnCenter_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(174, 12);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 3;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(255, 12);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(75, 23);
+            this.btnFont.TabIndex = 4;
+            this.btnFont.Text = "Font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // btnBold
+            // 
+            this.btnBold.Location = new System.Drawing.Point(336, 12);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(75, 23);
+            this.btnBold.TabIndex = 5;
+            this.btnBold.Text = "Bold";
+            this.btnBold.UseVisualStyleBackColor = true;
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.Location = new System.Drawing.Point(417, 12);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(75, 23);
+            this.btnItalic.TabIndex = 6;
+            this.btnItalic.Text = "Italic";
+            this.btnItalic.UseVisualStyleBackColor = true;
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            // 
+            // btnUnderline
+            // 
+            this.btnUnderline.Location = new System.Drawing.Point(498, 12);
+            this.btnUnderline.Name = "btnUnderline";
+            this.btnUnderline.Size = new System.Drawing.Size(75, 23);
+            this.btnUnderline.TabIndex = 7;
+            this.btnUnderline.Text = "Underline";
+            this.btnUnderline.UseVisualStyleBackColor = true;
+            this.btnUnderline.Click += new System.EventHandler(this.btnUnderline_Click);
+            // 
+            // comboBoxLanguages
+            // 
+            this.comboBoxLanguages.FormattingEnabled = true;
+            this.comboBoxLanguages.Location = new System.Drawing.Point(579, 12);
+            this.comboBoxLanguages.Name = "comboBoxLanguages";
+            this.comboBoxLanguages.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxLanguages.TabIndex = 8;
+            this.comboBoxLanguages.SelectedIndexChanged += new System.EventHandler(this.comboBoxLanguages_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxLanguages);
+            this.Controls.Add(this.btnUnderline);
+            this.Controls.Add(this.btnItalic);
+            this.Controls.Add(this.btnBold);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnCenter);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Text Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Lab7/Lab7/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Завантаження мов інтерфейсу
+            LoadLanguages();
+        }
+
+        // Завантаження мов інтерфейсу
+        private void LoadLanguages()
+        {
+            comboBoxLanguages.Items.Add("English");
+            comboBoxLanguages.Items.Add("Українська");
+            comboBoxLanguages.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Встановлення шрифту за замовчуванням
+            richTextBox1.Font = new Font("Arial", 12);
+        }
+
+        // Зміна мови інтерфейсу
+        private void comboBoxLanguages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxLanguages.SelectedItem.ToString() == "Українська")
+            {
+                ChangeLanguage("uk");
+            }
+            else
+            {
+                ChangeLanguage("en");
+            }
+        }
+
+        private void ChangeLanguage(string lang)
+        {
+            // Встановлення культури для зміни мови
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(lang);
+
+            // Оновлення всіх елементів форми
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
+            resources.ApplyResources(this, "$this");
+            ApplyResources(this, resources);
+        }
+
+        private void ApplyResources(Control control, ComponentResourceManager resources)
+        {
+            foreach (Control c in control.Controls)
+            {
+                resources.ApplyResources(c, c.Name);
+                if (c.Controls.Count > 0)
+                {
+                    ApplyResources(c, resources);
+                }
+            }
+        }
+
+        // Вирівнювання тексту
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void btnCenter_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        // Зміна шрифту тексту
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog.Font;
+            }
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            ToggleFontStyle(FontStyle.Bold);
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            ToggleFontStyle(FontStyle.Italic);
+        }
+
+        private void btnUnderline_Click(object sender, EventArgs e)
+        {
+            ToggleFontStyle(FontStyle.Underline);
+        }
+
+        private void ToggleFontStyle(FontStyle style)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                Font currentFont = richTextBox1.SelectionFont;
+                FontStyle newFontStyle;
+
+                if (richTextBox1.SelectionFont.Bold && style == FontStyle.Bold)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else if (richTextBox1.SelectionFont.Italic && style == FontStyle.Italic)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else if (richTextBox1.SelectionFont.Underline && style == FontStyle.Underline)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else
+                {
+                    newFontStyle = richTextBox1.SelectionFont.Style | style;
+                }
+
+                richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+            }
+        }
+    }
+}
